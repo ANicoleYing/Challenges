2ec336ef9a7cf1f0f335c12a0c458d1b9ecbb16c
Amber Nicole Ying <73565172+ANicoleYing@users.noreply.github.com>
2020-11-21 22:21:39 -0500

    Add files via upload

---
+++ WeekOneChallenges/WeekTwoChallenges/Greeter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekTwoChallenges
+{
+    class Greeter
+    {
+
+        public string SayHello(string Name)
+        {
+            string Greeting = "Hello " + "Oliver";
+            return Greeting;
+        }
+
+
+        public string SayFarewell(string Name)
+        {
+            string Farewell = ("Until we meet again, " + "Oliver");
+            return Farewell;
+        }
+
+
+        public string TimeOfDayGreeting(string Greeting)
+        {
+            string GoodEvening = ("Good eveniing!");
+            return GoodEvening;
+        }
+
+    }
+}
